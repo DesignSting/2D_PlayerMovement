11189b19597fb3c3fcec82c6ac82eae51e785155
Drue <waterman.drue.r@gmail.com>
2020-02-19 16:15:02 +1100

    Basically finished

---
+++ Assets/Scripts/ColliderColour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColliderColour : MonoBehaviour
+{
+    public Color colliderColour;
+    private Collider2D thisCollider;
+
+    private void Start()
+    {
+        thisCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = colliderColour;
+        float xSize = thisCollider.bounds.size.x;
+        float ySize = thisCollider.bounds.size.y;
+        Gizmos.DrawCube(thisCollider.bounds.center, new Vector3(xSize, ySize, 1));
+    }
+}
